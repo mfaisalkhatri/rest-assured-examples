00b9d6f4ad4ded4df275d572e993b5c529388d3f
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2021-11-20 15:28:38 +0530

    added put requests tests

---
+++ src/test/java/io/github/mfaisalkhatri/TestPutRequests.java
@@ -0,0 +1,71 @@
+package io.github.mfaisalkhatri;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import io.restassured.http.ContentType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Created By Faisal Khatri on 20-11-2021
+ */
+public class TestPutRequests {
+
+    Logger                      log = LogManager.getLogger (TestPutRequests.class);
+    private static final String URL = "https://reqres.in";
+
+    /**
+     * Created By Faisal Khatri on 20-11-2021
+     *
+     * @return test data for put requests
+     */
+    @DataProvider (name = "putData")
+    public Iterator<Object []> putData () {
+        final List<Object []> putData = new ArrayList<> ();
+        putData.add (new Object [] { 2, "Michael", "QA Lead" });
+        putData.add (new Object [] { 958, "Yuan", "Project Architect" });
+        return putData.iterator ();
+    }
+
+    /**
+     * Created By Faisal Khatri on 20-11-2021 Executing Put Request using Rest
+     * Assured.
+     *
+     * @param id
+     * @param name
+     * @param job
+     */
+    @Test (dataProvider = "putData")
+    public void putRequestsTests (final int id, final String name, final String job) {
+
+        final PostData postData = new PostData (name, job);
+        final String response = given ().contentType (ContentType.JSON)
+            .body (postData)
+            .when ()
+            .put (URL + "/api/users/" + id)
+            .then ()
+            .assertThat ()
+            .statusCode (200)
+            .and ()
+            .assertThat ()
+            .body ("name", equalTo (name))
+            .and ()
+            .assertThat ()
+            .body ("job", equalTo (job))
+            .and ()
+            .extract ()
+            .response ()
+            .body ()
+            .asString ();
+
+        this.log.info (response);
+
+    }
+}
