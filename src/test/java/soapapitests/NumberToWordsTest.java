42959c69340a952964b7216a97064a46d6dc80c7
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2022-08-28 21:48:34 +0530

    added soap api test exmaples

---
+++ src/test/java/soapapitests/NumberToWordsTest.java
@@ -0,0 +1,51 @@
+package soapapitests;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.config.XmlPathConfig;
+import org.testng.annotations.Test;
+
+/**
+ * @author Faisal Khatri
+ * @since 8/28/2022
+ **/
+public class NumberToWordsTest {
+
+    private static final String BASE_URL = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso";
+
+    @Test
+    public void testNumberToWordsAPI () {
+        URI file = null;
+        try {
+            file = getClass ().getClassLoader ()
+                .getResource ("numbertowordrequest.xml")
+                .toURI ();
+        } catch (URISyntaxException e) {
+            throw new Error ("Error in URI syntax", e);
+        }
+        File body = new File (file);
+
+        String response = given ().when ()
+            .body (body)
+            .contentType ("text/xml; charset=utf-8")
+            .log ()
+            .all ()
+            .post (BASE_URL)
+            .then ()
+            .log ()
+            .all ()
+            .statusCode (200)
+            .extract ()
+            .response ()
+            .asString ();
+
+        XmlPath xmlpath = new XmlPath (response).using (new XmlPathConfig ("UTF-8"));
+        assertEquals (xmlpath.getString ("m:NumberToWordsResult"), "five thousand nine hundred and fifty eight ");
+    }
+}
