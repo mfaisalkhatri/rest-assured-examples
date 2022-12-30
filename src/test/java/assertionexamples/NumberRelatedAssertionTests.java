72b1b312c63ceaf894876a1cab5c72e7c1815c41
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2022-12-30 18:54:13 +0530

    added example code for assertions (#60)

---
+++ src/test/java/assertionexamples/NumberRelatedAssertionTests.java
@@ -0,0 +1,65 @@
+package assertionexamples;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Faisal Khatri
+ * @since 12/30/2022
+ **/
+public class NumberRelatedAssertionTests {
+
+    private static String URL = "https://reqres.in/api/users/";
+
+    @Test
+    public void testNumberAssertions () {
+
+        given ().when ()
+            .queryParam ("page", 2)
+            .get (URL)
+            .then ()
+            .statusCode (200)
+            .and ()
+            .assertThat ()
+            .body ("page", equalTo(2))
+            .body ("per_page", greaterThan (4))
+            .body ("per_page", greaterThanOrEqualTo (6))
+            .body ("total", lessThan (14))
+            .body ("total_pages", lessThanOrEqualTo (3));
+    }
+
+    @Test
+    public void testGreaterThanAssertions () {
+
+        given ().when ()
+            .queryParam ("page", 2)
+            .get (URL)
+            .then ()
+            .statusCode (200)
+            .and ()
+            .assertThat ()
+            .body ("per_page", greaterThan (4))
+            .body ("per_page", greaterThanOrEqualTo (6));
+    }
+
+
+    @Test
+    public void testLessThanAssertions () {
+
+        given ().when ()
+            .queryParam ("page", 2)
+            .get (URL)
+            .then ()
+            .statusCode (200)
+            .and ()
+            .assertThat ()
+            .body ("total", lessThan (14))
+            .body ("total_pages", lessThanOrEqualTo (3));
+    }
+}
