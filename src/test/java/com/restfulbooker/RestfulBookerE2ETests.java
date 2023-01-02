555d1074aa2ab24ad6cc7eaaa41a77e288421d11
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2023-01-02 20:19:29 +0530

    added responsespec and request spec example codes

--- src/test/java/com/restfulbooker/RestfulBookerE2ETests.java
+++ src/test/java/com/restfulbooker/RestfulBookerE2ETests.java
@@ -1,9 +1,12 @@
 package com.restfulbooker;
 
-import static data.restfulbooker.BookingDataBuilder.*;
+import static data.restfulbooker.BookingDataBuilder.getBookingData;
+import static data.restfulbooker.BookingDataBuilder.getPartialBookingData;
 import static data.restfulbooker.TokenBuilder.getToken;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 import data.restfulbooker.BookingData;
 import data.restfulbooker.PartialBookingData;
@@ -15,7 +18,6 @@ import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Step;
 import io.qameta.allure.Story;
-import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -44,7 +46,7 @@ public class RestfulBookerE2ETests extends BaseSetup {
     @Description ("Example test for creating new booking - Post request")
     @Severity (SeverityLevel.BLOCKER)
     @Story ("End to End tests using rest-assured")
-    @Step("Create new booking")
+    @Step ("Create new booking")
     public void createBookingTest () {
         bookingId = given ().body (newBooking)
             .when ()
@@ -54,15 +56,12 @@ public class RestfulBookerE2ETests extends BaseSetup {
             .and ()
             .assertThat ()
             .body ("bookingid", notNullValue ())
-            .body ("booking.firstname", equalTo (newBooking.getFirstname ()))
-            .body ("booking.lastname", equalTo (newBooking.getLastname ()))
-            .body ("booking.totalprice", equalTo (newBooking.getTotalprice ()))
-            .body ("booking.depositpaid", equalTo (newBooking.isDepositpaid ()))
-            .body ("booking.bookingdates.checkin", equalTo (newBooking.getBookingdates ()
-                .getCheckin ()))
-            .body ("booking.bookingdates.checkout", equalTo (newBooking.getBookingdates ()
-                .getCheckout ()))
-            .body ("booking.additionalneeds", equalTo (newBooking.getAdditionalneeds ()))
+            .body ("booking.firstname", equalTo (newBooking.getFirstname ()), "booking.lastname",
+                equalTo (newBooking.getLastname ()), "booking.totalprice", equalTo (newBooking.getTotalprice ()),
+                "booking.depositpaid", equalTo (newBooking.isDepositpaid ()), "booking.bookingdates.checkin", equalTo (
+                    newBooking.getBookingdates ()
+                        .getCheckin ()), "booking.bookingdates.checkout", equalTo (newBooking.getBookingdates ()
+                    .getCheckout ()), "booking.additionalneeds", equalTo (newBooking.getAdditionalneeds ()))
             .extract ()
             .path ("bookingid");
     }
@@ -71,22 +70,18 @@ public class RestfulBookerE2ETests extends BaseSetup {
     @Description ("Example test for retrieving a booking - Get request")
     @Severity (SeverityLevel.CRITICAL)
     @Story ("End to End tests using rest-assured")
-    @Step("Get a the newly created booking")
+    @Step ("Get a the newly created booking")
     public void getBookingTest () {
         given ().get ("/booking/" + bookingId)
             .then ()
             .statusCode (200)
             .and ()
             .assertThat ()
-            .body ("firstname", equalTo (newBooking.getFirstname ()))
-            .body ("lastname", equalTo (newBooking.getLastname ()))
-            .body ("totalprice", equalTo (newBooking.getTotalprice ()))
-            .body ("depositpaid", equalTo (newBooking.isDepositpaid ()))
-            .body ("bookingdates.checkin", equalTo (newBooking.getBookingdates ()
-                .getCheckin ()))
-            .body ("bookingdates.checkout", equalTo (newBooking.getBookingdates ()
-                .getCheckout ()))
-            .body ("additionalneeds", equalTo (newBooking.getAdditionalneeds ()));
+            .body ("firstname", equalTo (newBooking.getFirstname ()), "lastname", equalTo (newBooking.getLastname ()),
+                "totalprice", equalTo (newBooking.getTotalprice ()), "depositpaid",
+                equalTo (newBooking.isDepositpaid ()), "bookingdates.checkin", equalTo (newBooking.getBookingdates ()
+                    .getCheckin ()), "bookingdates.checkout", equalTo (newBooking.getBookingdates ()
+                    .getCheckout ()), "additionalneeds", equalTo (newBooking.getAdditionalneeds ()));
 
     }
 
@@ -94,51 +89,44 @@ public class RestfulBookerE2ETests extends BaseSetup {
     @Description ("Example test for updating a booking - Put request")
     @Severity (SeverityLevel.NORMAL)
     @Story ("End to End tests using rest-assured")
-    @Step("Update the booking")
+    @Step ("Update the booking")
     public void updateBookingTest () {
         given ().body (updatedBooking)
             .when ()
-            .header ("Cookie", "token=" + generateToken ())
+            .header ("Cookie", "token=" + generateToken())
             .put ("/booking/" + bookingId)
             .then ()
             .statusCode (200)
             .and ()
             .assertThat ()
-            .body ("firstname", equalTo (updatedBooking.getFirstname ()))
-            .body ("lastname", equalTo (updatedBooking.getLastname ()))
-            .body ("totalprice", equalTo (updatedBooking.getTotalprice ()))
-            .body ("depositpaid", equalTo (updatedBooking.isDepositpaid ()))
-            .body ("bookingdates.checkin", equalTo (updatedBooking.getBookingdates ()
-                .getCheckin ()))
-            .body ("bookingdates.checkout", equalTo (updatedBooking.getBookingdates ()
-                .getCheckout ()))
-            .body ("additionalneeds", equalTo (updatedBooking.getAdditionalneeds ()));
-
+            .body ("firstname", equalTo (updatedBooking.getFirstname ()), "lastname",
+                equalTo (updatedBooking.getLastname ()), "totalprice", equalTo (updatedBooking.getTotalprice ()),
+                "depositpaid", equalTo (updatedBooking.isDepositpaid ()), "bookingdates.checkin", equalTo (
+                    updatedBooking.getBookingdates ()
+                        .getCheckin ()), "bookingdates.checkout", equalTo (updatedBooking.getBookingdates ()
+                    .getCheckout ()), "additionalneeds", equalTo (updatedBooking.getAdditionalneeds ()));
     }
 
     @Test
     @Description ("Example test for updating a booking partially- Patch request")
     @Severity (SeverityLevel.NORMAL)
     @Story ("End to End tests using rest-assured")
-    @Step("Update the booking partially")
+    @Step ("Update the booking partially")
     public void updatePartialBookingTest () {
         given ().body (partialUpdateBooking)
             .when ()
-            .header ("Cookie", "token=" + generateToken ())
+            .header ("Cookie", "token=" + generateToken())
             .patch ("/booking/" + bookingId)
             .then ()
             .statusCode (200)
             .and ()
             .assertThat ()
-            .body ("firstname", equalTo (partialUpdateBooking.getFirstname ()))
-            .body ("lastname", equalTo (updatedBooking.getLastname ()))
-            .body ("totalprice", equalTo (partialUpdateBooking.getTotalprice ()))
-            .body ("depositpaid", equalTo (updatedBooking.isDepositpaid ()))
-            .body ("bookingdates.checkin", equalTo (updatedBooking.getBookingdates ()
-                .getCheckin ()))
-            .body ("bookingdates.checkout", equalTo (updatedBooking.getBookingdates ()
-                .getCheckout ()))
-            .body ("additionalneeds", equalTo (updatedBooking.getAdditionalneeds ()));
+            .body ("firstname", equalTo (partialUpdateBooking.getFirstname ()), "lastname",
+                equalTo (updatedBooking.getLastname ()), "totalprice", equalTo (partialUpdateBooking.getTotalprice ()),
+                "depositpaid", equalTo (updatedBooking.isDepositpaid ()), "bookingdates.checkin", equalTo (
+                    updatedBooking.getBookingdates ()
+                        .getCheckin ()), "bookingdates.checkout", equalTo (updatedBooking.getBookingdates ()
+                    .getCheckout ()), "additionalneeds", equalTo (updatedBooking.getAdditionalneeds ()));
 
     }
 
@@ -146,9 +134,9 @@ public class RestfulBookerE2ETests extends BaseSetup {
     @Description ("Example test for deleting a booking - Delete request")
     @Severity (SeverityLevel.NORMAL)
     @Story ("End to End tests using rest-assured")
-    @Step("Delete the booking")
+    @Step ("Delete the booking")
     public void deleteBookingTest () {
-        given ().header ("Cookie", "token=" + generateToken ())
+        given ().header ("Cookie", "token=" + generateToken())
             .when ()
             .delete ("/booking/" + bookingId)
             .then ()
@@ -159,7 +147,7 @@ public class RestfulBookerE2ETests extends BaseSetup {
     @Description ("Example test for checking if booking is deleted by retrieving a deleted booking - Get request")
     @Severity (SeverityLevel.NORMAL)
     @Story ("End to End tests using rest-assured")
-    @Step("Check by retrieving deleted booking")
+    @Step ("Check by retrieving deleted booking")
     public void checkBookingIsDeleted () {
         given ().get ("/booking/" + bookingId)
             .then ()
@@ -171,10 +159,8 @@ public class RestfulBookerE2ETests extends BaseSetup {
             .when ()
             .post ("/auth")
             .then ()
-            .statusCode (200)
-            .and ()
             .assertThat ()
-            .body ("token", Matchers.not (nullValue ()))
+            .body ("token", is (notNullValue ()))
             .extract ()
             .path ("token");
     }
