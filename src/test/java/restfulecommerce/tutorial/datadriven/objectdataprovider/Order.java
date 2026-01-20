831be60be3a1a44092f2a662c16a3e1db3c26578
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2026-01-20 15:30:31 +0400

    added example to add orders using object data provider

---
+++ src/test/java/restfulecommerce/tutorial/datadriven/objectdataprovider/Order.java
@@ -0,0 +1,26 @@
+package restfulecommerce.tutorial.datadriven.objectdataprovider;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Order {
+    @JsonProperty ("user_id")
+    private String userId;
+    @JsonProperty ("product_id")
+    private String productId;
+    @JsonProperty ("product_name")
+    private String productName;
+    @JsonProperty ("product_amount")
+    private int    productAmount;
+    private int    qty;
+    @JsonProperty ("tax_amt")
+    private int    taxAmt;
+    @JsonProperty ("total_amt")
+    private int    totalAmt;
+    private int    expectedStatus;
+}
