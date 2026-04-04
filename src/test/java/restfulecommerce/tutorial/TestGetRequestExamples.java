5d2629ed66f33649cc63437ed753223fa8825767
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2026-04-04 16:41:14 +0530

    added examples for query and path params

--- src/test/java/restfulecommerce/tutorial/TestGetRequestExamples.java
+++ src/test/java/restfulecommerce/tutorial/TestGetRequestExamples.java
@@ -1,6 +1,10 @@
 package restfulecommerce.tutorial;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.testng.annotations.Test;
 
@@ -24,6 +28,74 @@ public class TestGetRequestExamples {
             .then ()
             .log ()
             .all ()
+            .statusCode (200)
+            .and ()
+            .body ("orders[0].id", equalTo (1));
+    }
+
+    @Test
+    public void testGetOrderWithMultipleQueryParam () {
+        given ().when ()
+            .log ()
+            .all ()
+            .queryParams ("id", 1, "user_id", "1", "product_id", "1")
+            .get ("http://localhost:3004/getOrder")
+            .then ()
+            .log ()
+            .all ()
+            .statusCode (200)
+            .and ()
+            .body ("orders[0].id", equalTo (1));
+    }
+
+    @Test
+    public void testGetOrderWithMultipleQueryParameters () {
+        given ().when ()
+            .log ()
+            .all ()
+            .queryParam ("id", 1)
+            .queryParam ("user_id", "1")
+            .queryParam ("product_id", "1")
+            .get ("http://localhost:3004/getOrder")
+            .then ()
+            .log ()
+            .all ()
+            .statusCode (200)
+            .and ()
+            .body ("orders[0].id", equalTo (1));
+    }
+
+    @Test
+    public void testGetOrderWithMultipleQueryParamWithMap () {
+        Map<String, Object> queryParams = new HashMap<> ();
+        queryParams.put ("id", 1);
+        queryParams.put ("user_id", "1");
+        queryParams.put ("product_id", "1");
+
+        given ().when ()
+            .log ()
+            .all ()
+            .queryParams (queryParams)
+            .get ("http://localhost:3004/getOrder")
+            .then ()
+            .log ()
+            .all ()
+            .statusCode (200)
+            .and ()
+            .body ("orders[0].id", equalTo (1));
+    }
+
+    @Test
+    public void testGetBookingWithPathParam () {
+        given ().when ()
+            .log ()
+            .all ()
+            .pathParam ("id", 3)
+            .get ("https://restful-booker.herokuapp.com/booking/{id}")
+            .then ()
+            .log ()
+            .all ()
             .statusCode (200);
     }
+
 }
