9b5dfd2aafb62e771133880bcd70c41c06c928e0
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2026-06-04 23:42:13 +0530

    fixed get tests with request and response spec

--- src/test/java/restful/ecommerce/TestGetRequestWithRestAssuredSpecs.java
+++ src/test/java/restful/ecommerce/TestGetRequestWithRestAssuredSpecs.java
@@ -38,12 +38,14 @@ public class TestGetRequestWithRestAssuredSpecs extends SetupSpecification {
     @Severity (SeverityLevel.CRITICAL)
     @Story ("Writing API Tests using rest assured configurations")
     public void getRequestTestWithRestAssuredConfig () {
+        final int orderId = 3;
         given ().when ()
-            .get ("/api/users/2")
+            .queryParam ("id", orderId)
+            .get ("/getOrder")
             .then ()
             .statusCode (200)
             .and ()
             .assertThat ()
-            .body ("data.first_name", equalTo ("Janet"));
+            .body ("orders[0].id", equalTo (orderId), "orders[0].product_name", equalTo ("USB-C Charger"));
     }
 }
